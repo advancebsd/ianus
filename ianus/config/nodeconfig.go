6566c56bfa41e75f603f45413b3794de1233c1ef
Todd Martin <kj4ntv@gmail.com>
2021-12-18 00:08:59 -0800

    Initial structures for nodes

---
+++ ianus/config/nodeconfig.go
@@ -0,0 +1,27 @@
+package config
+
+type network_info struct {
+	ip string
+	port int16
+}
+
+type gemini struct {
+	root string
+	net_info []network_info
+}
+
+type http struct {
+	root string
+	net_info []network_info
+}
+
+type Node struct {
+	hostname string
+	alias []string
+	gemini gemini
+	http http
+}
+
+type Nodes struct {
+	nodes map[string]Node
+}
