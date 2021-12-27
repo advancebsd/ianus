cb1169404b3b3346af2d58cbbc686d61950a38fd
Todd Martin <33560895+martintc@users.noreply.github.com>
2021-12-26 21:49:41 -0800

    Markdown analyzer and initial github workflow for unit tests (#2)
    * Delete placeholder files
    
    Will be using Go for this project, so this project structure does not make much sense
    
    * Initial main function with parsing command line arguments
    
    * Change file structure
    
    * Initial structures for nodes
    
    * Add TODO
    
    * Base implementation of Markdown lexical analyzer (#1)
    
    * Some initial implementation of Static generator and Markdown lexing
    
    * Implement some boilerplate functions
    
    * Write test for intializing, reading character and skipping whitespace
    
    * Add a token for content
    
    * WIP token state machine
    
    Untested, but currently has ability to read header tokens.
    TODO write a test case for checking state machine's ability to get header tokens
    
    * Implement reading header tokens and move function
    
    State machine can now properly read header tokens
    NextToken function moved to bottom as state machine will be large. Better readability
    
    * Implement test for state machine when reading header tokens
    
    * Add EOF and INVALID tokens
    
    * Make changes to test out and be able to run tests for github actions
    
    * First try to github actions for tests
    
    * Implement tokens '[]()!` ```' and relevant testing for lexing those tokens
    
    * fix the token to the proper token
    
    * Add a default case to mark unknown tokens as invalid
    
    * Make corrections, implement token lexing and some test cases
    
    * WIP: Implementing and testing escape tokens
    
    * Change to only lex and test for new line token
    
    * Implement reading in content and test case
    
    * WIP: Reading in astericks is an issue with tokenizing
    
    For some reason, header tokens are read properly, but not sequential astericks
    in a string
    
    * Complete the lexing of content and fixed errors with reading astericks
    
    Needed to return the token content inside of it's switch case, if not, the lexer
    would read another char ahead, messing with where the current position in the
    input was
    
    * Write a test case for lexing the bullet point symbols
    
    * Implement parsing of different tokens starting with '[' and relevant testing
    
    * Add a comment for a short function description
    
    * Test against a link in markdown format
    
    Had to include some adjustments to parsing for content to allow for charcaters
    such as ':' and '/'
    
    * Add some short function description comments
    
    * Add question mark and exclamation marks to allowable list for parsing content
    
    * Implement a sample file and a test agaist that sample file for parsing markdown
    
    * correct token
    
    * Formating changes after running 'go fmt'
    
    * Delete ianus
    
    Unintentional upload of a compiled binary

---
+++ ianus/ianus.go
@@ -0,0 +1,57 @@
+package main
+
+/*
+ * TODO: Implement the following command line arguments
+ *       -h (help)
+ *       -v (verbose)
+ *       -t (test config)
+ *       -c (alt config file location)
+ *       -l (alt log file location)
+ *
+ * TODO: Implement parsing configurations
+ *       Parse nodes and configurations
+ *
+ * TODO: Static generator implementation
+ *       Generate gemtext from markdown
+ *       Generate html from gemtext
+ *
+ * TODO: Implement LRU caching
+ *
+ * TODO: Server instances
+ *       Gemini Server over TLS
+ *       HTTP server
+ */
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	helpPtr := flag.Bool("h", false, "help")
+	verbosePtr := flag.Bool("v", false, "verbose")
+	testPtr := flag.Bool("t", false, "test config")
+	configPtr := flag.String("c", "~/.config/ianus.conf", "location of config file")
+	logPtr := flag.String("l", "/var/log/ianus", "location of log files")
+
+	flag.Parse()
+
+	/* temporary for testing */
+	fmt.Println("h: ", *helpPtr)
+	fmt.Println("v: ", *verbosePtr)
+	fmt.Println("t: ", *testPtr)
+	fmt.Println("c: ", *configPtr)
+	fmt.Println("l: ", *logPtr)
+
+	if *helpPtr {
+		fmt.Println("Ianus server help")
+		fmt.Println("\th - help")
+		fmt.Println("\tv - verbose")
+		fmt.Println("\tt - test configure file")
+		fmt.Println("\tc <file_location> - specify alternative configuration file")
+		fmt.Println("\tl <directory_location> - specify alternative log directory")
+		os.Exit(0)
+	}
+
+}
