320d4c4b406f8e397aec11b57bd7039ef76cb50b
Todd Martin <kj4ntv@gmail.com>
2021-12-29 01:43:33 -0800

    Add support for more tokens and test cases

--- gemtextRender/gemtextRender.go
+++ gemtextRender/gemtextRender.go
@@ -1,15 +1,73 @@
 package gemtextRender
 
 import (
+	"errors"
 	markdownLexer "github.com/advancebsd/ianus/markdownLexer"
 )
 
+type GemtextRender struct {
+	tokenStream []markdownLexer.Token
+	idx int
+	page string
+}
+
+func (g *GemtextRender) InitializeGemtextRender(ts []markdownLexer.Token) {
+	g.tokenStream = ts
+	g.idx = 0
+}
+
+func (g *GemtextRender) incrementIndex() bool {
+	if g.idx >= len(g.tokenStream) {
+		return false
+	}
+	g.idx += 1
+	return true
+}
+
+func (g *GemtextRender) readToken() (markdownLexer.Token, error) {
+	if g.idx >= len(g.tokenStream) {
+		var t markdownLexer.Token
+		t.Type = markdownLexer.INVALID
+		t.Literal = ""
+		return t, errors.New("No more tokens to read")
+	}
+	return g.tokenStream[g.idx], nil
+}
+
+func (g *GemtextRender) RenderDocument() (string, error) {
+	var gemtext string
+	var curr_token markdownLexer.Token
+	var err error
+	curr_token, err = g.readToken()
+	if err != nil {
+		return "", errors.New("No tokens to read")
+	}
+	for curr_token.Type != markdownLexer.EOF {
+		gemtext += g.renderMdTokenToGemtext(curr_token)
+		curr_token, err = g.readToken()
+		if err != nil {
+			break
+		}
+	}
+	return gemtext, nil
+}
+
+func (g *GemtextRender) peekNextToken() (markdownLexer.Token, error) {
+	if g.idx >= len(g.tokenStream) {
+		var t markdownLexer.Token
+		t.Type = markdownLexer.INVALID
+		t.Literal = ""
+		return t, errors.New("End of token stream")
+	}
+	return g.tokenStream[g.idx+1], nil
+}
+
 /**
  * TODO: Implement render to gemtext
- * TODO: Manage module imports
+ *       TODO: Handle rendering of brackets for links
  */
 
-func renderMdTokenToGemtext(t markdownLexer.Token) string {
+func (g *GemtextRender) renderMdTokenToGemtext(t markdownLexer.Token) string {
 	var str string
 	switch t.Type {
 	case markdownLexer.HEADER_ONE:
@@ -18,7 +76,45 @@ func renderMdTokenToGemtext(t markdownLexer.Token) string {
 		str =  "## "
 	case markdownLexer.HEADER_THREE:
 		str = "### "
+	case markdownLexer.INLINE_CODE:
+		str = t.Literal
+	case markdownLexer.CODE_BLOCK:
+		str = t.Literal
+	case markdownLexer.BULLET_MINUS:
+		str = "* "
+	case markdownLexer.BULLET_PLUS:
+		str = "* "
+	case markdownLexer.UNCHECKED:
+		str = t.Literal
+	case markdownLexer.CHECKED:
+		str = t.Literal
+	case markdownLexer.QUOTE:
+		str = t.Literal
+	case markdownLexer.RIGHT_BRACKET:
+
+	case markdownLexer.LEFT_BRACKET:
+
+	case markdownLexer.RIGHT_PAREN:
+
+	case markdownLexer.LEFT_PAREN:
+
+	case markdownLexer.ITALIC:
+		str = ""
+	case markdownLexer.BOLD:
+		str = ""
+	case markdownLexer.BOLD_ITALIC:
+		str = ""
+	case markdownLexer.HORIZONTAL_RULE:
+		str = t.Literal
+	case markdownLexer.CONTENT:
+		str = t.Literal
+	case markdownLexer.NEW_LINE:
+		str = string('\n')
+	default:
+		str = ""
 	}
+
+	g.incrementIndex()
 
 	return str
 }
--- gemtextRender/gemtextRender_test.go
+++ gemtextRender/gemtextRender_test.go
@@ -1,16 +1,90 @@
 package gemtextRender
 
 import (
-	"testing"
 	markdownLexer "github.com/advancebsd/ianus/markdownLexer"
+	"testing"
+	"fmt"
 )
 
 func TestRenderHeaderTokens(t *testing.T) {
-	var token markdownLexer.Token
-	token.Type = markdownLexer.HEADER_ONE
-	var str string
-	str = renderMdTokenToGemtext(token)
-	if str != "# " {
+	var err error
+	str := "# ## ###"
+	l := new(markdownLexer.Lexer)
+	l.InitializeLexer(str)
+	var tokens []markdownLexer.Token
+	var tok markdownLexer.Token
+	tok = l.NextToken()
+	for tok.Type != markdownLexer.EOF {
+		tokens = append(tokens, tok)
+		tok = l.NextToken()
+	}
+	g := new(GemtextRender)
+	g.InitializeGemtextRender(tokens)
+	tok, err = g.readToken()
+	if err != nil {
+		t.Errorf("Issue reading token in sample")
+	}
+	tok, err = g.readToken()
+	if g.renderMdTokenToGemtext(tok) != "# " {
 		t.Errorf("Did not properly render HEADER_ONE token")
+	}
+	tok, err = g.readToken()
+	if g.renderMdTokenToGemtext(tok) != "## " {
+		t.Errorf("Did not properly render HEADER_TWO token")
+	}
+	tok, err = g.readToken()
+	if g.renderMdTokenToGemtext(tok) != "### " {
+		t.Errorf("Did not properly render HEADER_THREE token")
+	}
+}
+
+func TestRenderBulletPoints(t *testing.T) {
+	var err error
+	str := "+ -"
+	l := new(markdownLexer.Lexer)
+	l.InitializeLexer(str)
+	var tokens []markdownLexer.Token
+	var tok markdownLexer.Token
+	tok = l.NextToken()
+	for tok.Type != markdownLexer.EOF {
+		tokens = append(tokens, tok)
+		tok = l.NextToken()
+	}
+	g := new(GemtextRender)
+	g.InitializeGemtextRender(tokens)
+	tok, err = g.readToken()
+	if err != nil {
+		t.Errorf("Issue reading token during test")
+	}
+	tok, err = g.readToken()
+	if g.renderMdTokenToGemtext(tok) != "* " {
+		t.Errorf("Did not properly render BULLET_PLUS token")
+	}
+	tok, err = g.readToken()
+	if g.renderMdTokenToGemtext(tok) != "* " {
+		t.Errorf("Did not properly render BULLET_MINUS token")
+	}
+}
+
+func TestRenderOnString(t *testing.T) {
+	str := "# HeaderOne\nSomeInformation about test\n *HelloWorld*"
+	l := new(markdownLexer.Lexer)
+	l.InitializeLexer(str)
+	var tokens []markdownLexer.Token
+	var tok markdownLexer.Token
+	tok = l.NextToken()
+	for tok.Type != markdownLexer.EOF {
+		tokens = append(tokens, tok)
+		tok = l.NextToken()
+	}
+	g := new(GemtextRender)
+	g.InitializeGemtextRender(tokens)
+	result, err := g.RenderDocument()
+	if err != nil {
+		t.Errorf("Not properly reading tokens from lexer")
+	}
+	expected := "# HeaderOne\nSomeInformation about test\nHelloWorld"
+	if result != expected {
+		t.Errorf("Did no properly render to gemtext the test string")
 	}
 }
